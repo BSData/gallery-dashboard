6200687772ceec06f382136b87d93f36fbab6d23
Amadeusz Sadowski <amis.tau.92@gmail.com>
2022-09-02 08:49:25 +0200

    rename to GalleryDashboard

--- src/BSData.GalleryDashboard/Program.cs
+++ src/BSData.GalleryDashboard/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BSData.StatusDashboard;
+using BSData.GalleryDashboard;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
