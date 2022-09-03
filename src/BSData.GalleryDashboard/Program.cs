b441c1d2d887c052cb769ae0d7bd86834c420ba1
Amadeusz Sadowski <amis.tau.92@gmail.com>
2022-09-03 15:00:26 +0200

    feat: add gallery grid view

--- src/BSData.GalleryDashboard/Program.cs
+++ src/BSData.GalleryDashboard/Program.cs
@@ -1,10 +1,13 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using BSData.GalleryDashboard;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddOptions<GalleryBrowserOptions>().BindConfiguration("GalleryBrowser");
+builder.Services.AddSingleton<GalleryBrowserState>();
+builder.Services.AddScoped<GalleryHttpClient>();
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 
