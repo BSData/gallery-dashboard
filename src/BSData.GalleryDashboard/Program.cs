7277fc5de96a70d23b4dab75b4f604f6423f6ac0
Amadeusz Sadowski <amis.tau.92@gmail.com>
2023-11-27 21:33:49 +0100

    feat: migrate to .NET 8 and FluentUI v4

--- src/BSData.GalleryDashboard/Program.cs
+++ src/BSData.GalleryDashboard/Program.cs
@@ -1,16 +1,12 @@
-#pragma warning disable CA1852
-
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Fast.Components.FluentUI;
+using Microsoft.FluentUI.AspNetCore.Components;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-#pragma warning disable IL2026 // BindConfiguration RequiresUnreferencedCodeAttribute TOptions's dependent types may have their members trimmed. Ensure all required members are preserved.
 builder.Services.AddOptions<GalleryBrowserOptions>().BindConfiguration("GalleryBrowser");
-#pragma warning restore IL2026
 builder.Services.AddSingleton<GalleryBrowserState>();
 builder.Services.AddScoped<GalleryHttpClient>();
 builder.Services.AddFluentUIComponents();
